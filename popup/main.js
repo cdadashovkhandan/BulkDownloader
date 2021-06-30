be57f8994dbad953f91bfe8ebca502601e19edc4
cdadashovkhandan <chingizdkhandan@gmail.com>
2021-06-30 14:31:17 +0200

    basic filtered list of tabs that contain files

--- popup/main.js
+++ popup/main.js
@@ -1,12 +1,19 @@
+// extra debug output
+var debug = true;
+
+// basic error handling
+function onError(error) { console.log(`Error: ${error}`);}
+
+// get all the current window's tabs immediately upon activation
+getTabs(true);
+
 // holy god jesus I have no idea how JQuery works
 var downloadButton = document.getElementById('downloadButton');
-// onClick's logic below:
-downloadButton.addEventListener('click', function() {
-    getTabs(true);
-    console.log("download button clicked");
-});
 
-console.log("BULKDOWNLOADER STARTED")
+// download button logic
+downloadButton.addEventListener('click', function() {
+    console.log("boop");
+});
 
 // Save a file from a given URL
 // TODO: might be dumb to do this per file, maybe better to do it in bulk, like a list of URLs
@@ -15,27 +22,47 @@ function saveFile(url, destinationPath) {
     console.log("Saving file from " + url);
 }
 
+
+// Filter tabs to only the ones that contain files, then display them.
+// FIXME: detects unnecessary tabs like .html and .php
+function filterTabs(tabs) { 
+    const regex = new RegExp("\\..{3,4}\\b$")
+    
+    var newTabs = [];
+    for (let tab of tabs) {
+        console.log("[DEBUG] TAB URL IS " + tab.url);
+        if(regex.test(tab.url)) newTabs.push(tab);
+    }
+    
+    if (debug) {
+        console.log("------TABS DETECTED------")
+        for (let tab of newTabs) {
+            console.log(tab.url);
+        }
+    }
+
+    // insert tabs into table
+    for (let tab of newTabs) {
+        var table = document.getElementById("backdrop");
+        var row = table.insertRow();
+
+        var cell = row.insertCell()
+        cell.className = "tabData";
+        cell.innerHTML = tab.title;
+
+        cell.addEventListener('click', function() {
+            console.log("beep");
+            // TODO: add to list of active tabs
+            // global var perhaps?
+        });
+    }
+}
+
 // retrieve all the current tabs that are open for the user
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/getAll
 function getTabs(currentOnly) {
-    console.log("getTabs called");
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/Window
-    var browserWindows;
-    
-    // get a collection of all browser windows, or only the current one
-    if (currentOnly) {
-        browserWindows = browser.windows.getCurrent();
-    } else {
-        browserWindows = browser.windows.getAll(true);
-    }
-    var allTabs;
 
-    // build list of all tabs
-    browserWindows.forEach(function (window) {
-        allTabs.push(...window.tabs);
-    })
+    let querying = browser.tabs.query({currentWindow:  currentOnly});
 
-    alltabs.forEach(tab => console.log(tab.title))
-
-
+    querying.then(filterTabs, onError)
 }
