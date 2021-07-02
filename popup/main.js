4ec337c1dee403c76c8c470eca4f2ce45ecb271a
cdadashovkhandan <chingizdkhandan@gmail.com>
2021-07-02 16:24:25 +0200

    add thumbnails, clean up some CSS, add format detection exclusions

--- popup/main.js
+++ popup/main.js
@@ -2,6 +2,7 @@
 var debug = true;
 var activeTabs = []
 var newTabs = []
+var excludedFormats = [".asp", ".html", ".htm", ".php"]
 // basic error handling
 function onError(error) { console.log(`Error: ${error}`);}
 
@@ -26,13 +27,15 @@ downloadButton.addEventListener('click', async function() {
 
 
 // Filter tabs to only the ones that contain files, then display them.
-// FIXME: detects unnecessary tabs like .html and .php
 function filterTabs(tabs) { 
     const regex = new RegExp("\\..{3,4}\\b$")
     
     for (let tab of tabs) {
         console.log("[DEBUG] TAB URL IS " + tab.url);
-        if(regex.test(tab.url)) newTabs.push(tab);
+        var format = tab.url.match(regex);
+        console.log("format = " + format);
+        // TODO: might be a little bit naive with the magic number index
+        if(format != null && !excludedFormats.includes(format[0].toLowerCase()) ) newTabs.push(tab); 
     }
     
     if (debug) {
@@ -49,8 +52,10 @@ function filterTabs(tabs) {
 
         var cell = row.insertCell()
         cell.className = "tabData";
-        cell.innerHTML = tab.title;
-
+        cell.innerHTML = tab.title.substring(0,17) + "...";
+        var thumbnail = row.insertCell();
+        thumbnail.innerHTML = "<img src = \"" + tab.url + "\"></img>"
+        
         cell.addEventListener('click', function() {
             activeTabs.push(tab);
             cell.style.backgroundColor = "#34b4eb"
