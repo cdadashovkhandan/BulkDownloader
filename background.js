1b2cbef6afda5375a859144e7b20b75fa4242a01
cdadashovkhandan <chingizdkhandan@gmail.com>
2021-07-02 22:15:17 +0200

    pdf icon, select all button, groundwork for file path

--- background.js
+++ background.js
@@ -1,18 +1,24 @@
 // Save a file from a given URL
 // TODO: might be dumb to do this per file, maybe better to do it in bulk, like a list of URLs
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/downloads/download
-function saveFile(iurl, destinationPath) {
-    console.log("Saving file from " + iurl);
-    browser.downloads.download({
-      url : iurl,
+function saveFile(tab, destinationPath) {
+    var url = tab.url;
+    console.log("Saving file from " + url);
+    var options = {
+      url : url,
       conflictAction : 'uniquify'
-    });
+    }
+
+    if (destinationPath != "null" && destinationPath != null) options.filename = `${destinationPath}/${tab.fileName}`;
+    
+    browser.downloads.download(options);  
 }
 
 function handleMessage(request, sender, sendResponse) {
     console.log("message received " + request);
-    saveFile(request, "asdfsddf");
-    sendResponse({response: "ACK"}); // TODO: this is redundant
+    request = JSON.parse(request);
+    console.log("parsed request: " + JSON.stringify(request));
+    saveFile(request.tab, request.path);
   }
 
 browser.runtime.onMessage.addListener(handleMessage);
--- popup/main.js
+++ popup/main.js
@@ -1,6 +1,7 @@
 // extra debug output
 var debug = true;
 
+var allSelected = false;
 
 var selectedTabs = []
 var downloadableTabs = []
@@ -8,6 +9,7 @@ var downloadableTabs = []
 var excludedFormats = [".asp", ".html", ".htm", ".php"]
 var videoFormats = [".mp4", ".mkv", ".webm", ".ogg"]
 
+var destinationPath = null;
 // basic error handling
 function onError(error) { console.log(`Error: ${error}`);}
 
@@ -16,19 +18,43 @@ getTabs(true);
 
 // holy god jesus I have no idea how JQuery works
 var downloadButton = document.getElementById('downloadButton');
+var selectAllButton = document.getElementById('selectAllButton');
 
 // download button logic
 downloadButton.addEventListener('click', async function() {
-    console.log("boop");
+    console.log("Download button clicked");
     for (let tab of selectedTabs) {
         console.log(tab.url);
         try {
-            await browser.runtime.sendMessage(tab.url);
+            await browser.runtime.sendMessage(JSON.stringify({tab: tab, path: destinationPath}));
         } catch (error) {
             console.error("downloadButton error: " + error.message)
         }
     }
 });
+
+document.getElementById("fileInput").addEventListener("change", function(event) {
+    destinationPath = event.target;  
+});
+
+selectAllButton.addEventListener('click', function() {
+    console.log("All files selected");
+    if (allSelected) {
+        var selectedItems = document.getElementsByClassName("tabData selected");
+        while(selectedItems.length) {
+            selectedItems[0].className = "tabData deselected";
+        }
+        
+        selectedTabs = [];
+    } else {
+        var deselectedItems = document.getElementsByClassName("tabData deselected");
+        while(deselectedItems.length) {
+            deselectedItems[0].className = "tabData selected";
+        }
+        selectedTabs = [...downloadableTabs];
+    } 
+    allSelected = !allSelected;
+})
 
 function displayTab(tab, format) {
     var isVideo = videoFormats.includes(format); 
@@ -36,25 +62,27 @@ function displayTab(tab, format) {
     var row = table.insertRow();
 
     var cell = row.insertCell()
-    cell.className = "tabData";
-    cell.innerHTML = tab.title.substring(0,17) + "...";
-    cell.id = "deselected"
+    cell.className = "tabData deselected";
+    cell.innerHTML = tab.fileName;
+
+    // Thumbnail generation
     var thumbnail = row.insertCell();
     if (isVideo) {
         thumbnail.innerHTML = `<video> <source src="${tab.url}" type="video/${format.substring(1)}"></video>`;
+    } else if (format == ".pdf") { // generic thumbnail for pdf, as they don't have thumbnails on their own.
+        thumbnail.innerHTML = "<img src = \"pdficon.png\"></img>"
     } else {
         thumbnail.innerHTML = "<img src = \"" + tab.url + "\"></img>"
     }
     
     cell.addEventListener('click', function() {
-        if (cell.id == "selected") {
-            cell.id = "deselected";
+        if (cell.className == "tabData selected") {    
+        cell.className = "tabData deselected";
             selectedTabs.pop(tab);
-            cell.style.backgroundColor = "#354952";
         } else {
-            cell.id = "selected"
-            cell.style.backgroundColor = "#34b4eb";
+            cell.className = "tabData selected"
             selectedTabs.push(tab);
+            if (selectedTabs.length == downloadableTabs.length) allSelected = true;
         }
     });
 }
@@ -62,7 +90,7 @@ function displayTab(tab, format) {
 // Filter tabs to only the ones that contain files, then display them.
 function filterTabs(tabs) { 
     const regex = new RegExp("\\..{3,4}\\b$")
-    
+    const fileNameRegex = new RegExp("[a-zA-Z0-9_.]*\\..{3,4}$")
     for (let tab of tabs) {
         console.log("[DEBUG] TAB URL IS " + tab.url);
         var format = tab.url.match(regex);
@@ -74,6 +102,7 @@ function filterTabs(tabs) {
         // TODO: might be a little bit naive with the magic number index
         if (format != null && !excludedFormats.includes(format) )  {
             downloadableTabs.push(tab); 
+            tab.fileName = tab.url.match(fileNameRegex)[0];
             displayTab(tab, format)
         }
     }
